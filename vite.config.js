2e227dd82a58a1dd9ee7b5a12aeeb906ee0b6760
dm0114 <dm10802@gmail.com>
2022-10-11 03:38:43 +0900

    ah

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
+
 import VitePluginHtmlEnv from 'vite-plugin-html-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePluginHtmlEnv(),
     VitePluginHtmlEnv({
-      compiler: true
+      prefix: '<{',
+      suffix: '}>',
     })
   ]
 })
-
